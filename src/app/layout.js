ef50a0892e9f0fb73f6dd92c7d2721c1f5497b61
mkshov <aselya020407@gmail.com>
2023-12-21 19:03:25 +0600

    loading page

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ import Header from "@/components/Header/Header";
 import AOSWrapper from "@/utils/aosInit";
 import Footer from "@/components/Footer/Footer";
 import GTSSpeedDial from "@/components/SpeedDial";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
